57e9b835413d3b3bc7ecfd716c2776446fdef185
Will <corkillw@gmail.com>
2023-10-14 16:36:43 +0100

    Make modern header (see desc)
    SCSS has been compiled into a CSS file and used alongside Blade/HTML content to generate a modern header with a hamburger menu and an account access button.
    
    While not yet usable, the buttons will be made functional when the respective sections are created as in line with the Gantt chart.
    
    The hamburger menu will be completed tomorrow as it is the last day to be allocated on the Gantt chart for this topic. The footer will also be completed tomorrow in a matching style.

--- resources/views/components/structure/wrapper.blade.php
+++ resources/views/components/structure/wrapper.blade.php
@@ -1,13 +1,29 @@
 <x-structure.base :$title> {{-- Use the base structure before the header is made --}}
     <div class="wrapper">
         <header>
-            <div class="title">
-                <h1>Header placeholder content</h1>
+            <div id="menu-toggle" class="menu-toggle">
+                <a href="#" onclick="document.getElementById('menu-toggle').classList.toggle('active');">
+                    <svg width="30" height="30" viewBox="0 0 30 30">
+                        @for ($i = 0; $i < 3; $i++)
+                            <rect class="child-{{ $i }}" y="{{ $i * 13 }}" width="30" height="2" rx="1.5" fill="#1f1f1f" />
+                        @endfor
+                        <line class="child-3" x1="4" x2="26" y1="4" y2="26" stroke="#1f1f1f" stroke-width="2" stroke-linecap="round" />
+                        <line class="child-4" x1="4" x2="26" y1="26" y2="4" stroke="#1f1f1f" stroke-width="2" stroke-linecap="round" />
+                    </svg>
+                </a>
             </div>
-            <nav>
-                <a href="{{ url("/") }}">Home</a>
-            </nav>
+            <div class="title">
+                <h2>Online Learning Platform</h2>
+            </div>
+            <div class="user-account">
+                <a href="{{ url("/account") }}">
+                    <img width="50" height="50" src="{{ asset('assets/images/user-icon.svg') }}" alt="A button with an outline of a person for account access">
+                </a>
+            </div>
         </header>
+        <nav>
+            <a href="{{ url('/') }}">Home</a>
+        </nav>
         <main>
             {{ $slot }}
         </main>
