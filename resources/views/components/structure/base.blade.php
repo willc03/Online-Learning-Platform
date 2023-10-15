bf041eb8c1551ddcba4d9db8c580ca0f98c94d48
Will <corkillw@gmail.com>
2023-10-15 13:58:11 +0100

    Add page load transition blocker
    This will stop transitions from being visible when pages load, and will allow them to play after 1ms.

--- resources/views/components/structure/base.blade.php
+++ resources/views/components/structure/base.blade.php
@@ -1,13 +1,25 @@
 {{-- A file which provides to views the basic structure of an HTML page. --}}
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> {{-- This viewport tag allows for mobile optimisation. --}}
         <title>{{ $title }}</title> {{-- A title can be provided when the component is called --}}
-        <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}" {{-- Use a global stylesheet --}}
+        <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}"> {{-- Use a global stylesheet --}}
     </head>
-    <body>
+
+    <body class="block-transitions">
         {{ $slot }} {{-- The slot allows the component to be used as a HTML-like tag in Blade PHP files. --}}
     </body>
+
+    {{-- A function will be called to remove the block-transitions element from the body, this will stop odd transitions from appearing when the page loads --}}
+    <script>
+        (function() {
+            setTimeout(() => {
+                document.body.classList.remove("block-transitions");
+            }, 1);
+        })()
+    </script>
+
 </html>
