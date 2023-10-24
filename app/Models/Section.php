e0ff7bd089918a33eee619e7a50cfea27e8e61a6
Will <corkillw@gmail.com>
2023-10-24 14:20:41 +0100

    Create Section table and model
    The relationships are defined in the model file, whereas the structure is defined in the migration file.

---
+++ app/Models/Section.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    /*
+     * Create a many-to-one relationship between the section and its course
+     * (many sections can belong to a course)
+     */
+    public function course()
+    {
+        return $this->hasOne(Course::class);
+    }
+
+    /*
+     * Create a one-to-many relationship between a section and its items
+     * (one section can have many items)
+     */
+    public function items()
+    {
+        return $this->hasMany(SectionItem::class);
+    }
+}
---
+++ database/migrations/2023_10_24_130740_create_sections_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->integer('position');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
