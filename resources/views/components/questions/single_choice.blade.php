d26014442aea88b9873d2836cdefcfc6bdcd78c3
Will <corkillw@gmail.com>
2023-11-06 20:18:41 +0000

    Tidy single_choice_blade.php

--- resources/views/components/questions/single_choice.blade.php
+++ resources/views/components/questions/single_choice.blade.php
@@ -1,35 +1,24 @@
-{{-- Add a hidden attribute for the final submitted value --}}
-
-<input type="hidden" name="answer" id="answer">
-{{-- Display the options to the user --}}
-<fieldset class="single-choice-field">
+<input type="hidden" name="answer" id="answer"> {{-- Add a hidden attribute for the final submitted value --}}
+<fieldset class="single-choice-field"> {{-- Display the options to the user --}}
     <legend>Click your answer:</legend>
     @foreach($choices as $option) {{-- $value is passed in from the question page --}}
         <x-components.3d_button class="option-button" onclick="onSingleChoiceButtonClick(this, {{ $option }})">{{ $option }}</x-components.3d_button>
     @endforeach
 </fieldset>
-{{-- Display a submit button if they are allowed to change their answer --}}
-@if(!$oneTimeAnswer)
+
+@if(!$oneTimeAnswer) {{-- Display a submit button if they are allowed to change their answer --}}
     <input type="submit" id="submit-question" disabled>
 @endif
 
-
-{{-- Write a script to manage the clicking of correct answers --}}
-
-<script>
-    {{-- Get all the buttons --}}
+<script> {{-- Write a script to manage the clicking of correct answers --}}
     const buttons = document.querySelectorAll(".option-button");
 
     function onSingleChoiceButtonClick(selectedObject, optionValue)
     {
-        {{-- Loop through all the buttons and remove the selected attribute --}}
-        buttons.forEach(button => {
-            button.classList.remove("selected");
-        })
-        {{-- Add the selected attribute to the clicked button --}}
-        selectedObject.classList.add("selected");
-        {{-- Set the answer hidden input to the value in the button --}}
-        document.getElementById("answer").setAttribute("value", optionValue);
+        buttons.forEach(button => button.classList.remove("selected")); {{-- Loop through the buttons and remove the 'selected' attribute --}}
+        selectedObject.classList.add("selected"); {{-- Add the 'selected' attribute to the clicked button --}}
+        document.getElementById("answer").setAttribute("value", optionValue); {{-- Set the answer hidden input to the value in the button --}}
+
         {{-- Enable the submit button if it exists --}}
         const submit = document.getElementById("submit-question");
         if (submit) {
@@ -37,9 +26,7 @@
         }
     }
 
-    @if(!$oneTimeAnswer)
-        buttons.forEach(button => {
-            button.setAttribute("type", "button");
-        })
+    @if(!$oneTimeAnswer) {{-- Set the button to not submit the form if it's set that way when called (unable to do this within the component) --}}
+        buttons.forEach(button => button.setAttribute("type", "button"));
     @endif
 </script>
