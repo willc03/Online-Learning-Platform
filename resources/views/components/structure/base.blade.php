1908abe8fe11801021b286d902f81d1155811aec
Will <corkillw@gmail.com>
2023-11-20 12:50:54 +0000

    Add jQuery source script

--- resources/views/components/structure/base.blade.php
+++ resources/views/components/structure/base.blade.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> {{-- This viewport tag allows for mobile optimisation. --}}
         <title>{{ $title }} - Online Learning Platform</title> {{-- A title can be provided when the component is called --}}
         <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}"> {{-- Use a global stylesheet --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     </head>
 
     <body class="block-transitions">
