21af74c5068261a5007de9f5d414ea8726bbe2b3
Will <corkillw@gmail.com>
2023-11-21 20:33:03 +0000

    Finalise the "fill in the blank" questions

--- resources/views/components/questions/fill_in_blanks.blade.php
+++ resources/views/components/questions/fill_in_blanks.blade.php
@@ -21,103 +21,91 @@
 {{-- Make a script to manage the filling of the blanks --}}
 <script>
     $(function() {
-        // Add cubic easing
-        $.easing.easeOutCubic = function (x, t, b, c, d) {
-            return c*((t=t/d-1)*t*t + 1) + b;
-        }
-        // Define initial variables
+        $.easing.easeOutCubic = function(x, t, b, c, d) { {{-- Define the cubic easing function --}}
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        };
+
+        {{-- Define jQuery class and id selectors --}}
         const blankFields = $(".blank");
         const optionButtons = $(".option-button");
         const answerInput = $("#answer");
-        const submitInput = $("#submit-question")
+        const submitInput = $("#submit-question");
 
-        // Functions
+        {{-- Define functions, these will not be commented as they are self-explanatory --}}
         function getFirstBlankField() {
-            return $(blankFields).not(".filled").first()[0] ?? false;
+            return blankFields.not(".filled").first()[0] || false;
         }
         function getElementDetails(element) {
+            const $element = $(element);
             return {
-                width: $(element).width(),
-                height: $(element).height(),
-                x: $(element).offset().left,
-                y: $(element).offset().top,
-            } ?? false;
+                width: $element.width(),
+                height: $element.height(),
+                x: $element.offset().left,
+                y: $element.offset().top,
+            };
         }
 
-        // Logic
-        // Option button logic
+        {{-- Option button logic --}}
         optionButtons.on("click", function(event) {
-            // Button details
-            let button = event.currentTarget;
-            let buttonDetails = getElementDetails(button);
+            const button = $(event.currentTarget); {{-- Way of retrieving the clicked button in jQuery --}}
+            const buttonDetails = getElementDetails(button);
 
-            // Blank field details
-            let blankField = getFirstBlankField();
-            if (!blankField) { return; }
+            if (button.hasClass("block-transitions")) { {{-- A check for whether the button is a currently selected option --}}
+                const filledField = $(".blank[option='" + button.attr("id") + "']");
+                filledField.removeClass("filled").attr("option", null);
+                button.removeClass("block-transitions").animate({ opacity: 0 }, 100, "easeOutCubic", function() {
+                    setTimeout(function() {
+                        button.css({ position: "", width: "", height: "", left: "", top: "", zIndex: "" })
+                            .animate({ opacity: 100 }, 100);
+                    }, 300, "easeOutCubic");
+                });
+            } else { {{-- Move the button to the first available option, if available --}}
+                const blankField = getFirstBlankField();
+                if (!blankField) return;
 
-            let fieldDetails = getElementDetails(blankField);
+                const fieldDetails = getElementDetails(blankField);
 
-            // Change field class
-            $(blankField).addClass("filled").attr("option", button.id);
+                $(blankField).addClass("filled").attr("option", button.attr("id"));
 
-            // Change button class
-            $(button).addClass("block-transitions");
+                $(button).addClass("block-transitions").css({
+                    position: "absolute",
+                    width: buttonDetails.width,
+                    height: buttonDetails.height,
+                    left: buttonDetails.x,
+                    top: buttonDetails.y,
+                    zIndex: 2,
+                }).animate({
+                    width: fieldDetails.width,
+                    height: fieldDetails.height,
+                    left: fieldDetails.x + 1,
+                    top: fieldDetails.y + 3,
+                }, 500, "easeOutCubic");
+            }
 
-            // Change CSS
-            $(button).css({
-                position: "absolute",
-                width: buttonDetails.width,
-                height: buttonDetails.height,
-                left: buttonDetails.x,
-                top: buttonDetails.y,
-                zIndex: 2,
-            })
+            {{-- Recalculate the answer value for the form --}}
+            const answer = blankFields.filter(".filled").map(function() {
+                return $("#" + $(this).attr("option")).val();
+            }).get();
 
-            // Animate new button location
-            $(button).animate({
-                width: fieldDetails.width,
-                height: fieldDetails.height,
-                left: fieldDetails.x + 1,
-                top: fieldDetails.y + 3
-            }, 500, "easeOutCubic")
-
-            // Animate the blank box disappearing
-            $(blankField).animate({
-                opacity: 0
-            }, 500)
-
-            // Recalculate the answer
-            let answer = [];
-            blankFields.each(function(_, field) {
-                let selectedOption = $(field).attr("option") || false;
-                if (selectedOption) {
-                    let optionElement = $("#" + selectedOption)[0] || false;
-                    if (optionElement) {
-                        answer.push(optionElement.value);
-                    }
-                }
-            });
-            answerInput.attr("value", JSON.stringify(answer));
-
-            $(submitInput)[0].disabled = answer.length !== $(blankFields).length;
+            answerInput.val(JSON.stringify(answer));
+            submitInput.prop("disabled", answer.length !== blankFields.length);
         });
 
-        $(window).on("resize", function() { {{-- The absolute elements must be moved when the window is resized! --}}
-            blankFields.each(function(_, field) {
-                let selectedOption = $(field).attr("option") || false;
+        {{-- Window logic, to move the absolute form buttons when the window size is changed. --}}
+        $(window).on("resize", function() {
+            blankFields.filter(".filled").each(function(_, field) {
+                const selectedOption = $(field).attr("option");
                 if (selectedOption) {
-                    let optionElement = $("#" + selectedOption)[0] || false;
-                    if (optionElement) {
-                        let fieldDetails = getElementDetails(field);
-                        $(optionElement).css({
-                            width: fieldDetails.width,
-                            height: fieldDetails.height,
-                            left: fieldDetails.x + 1,
-                            top: fieldDetails.y + 3
-                        });
-                    }
+                    const optionElement = $("#" + selectedOption);
+                    const fieldDetails = getElementDetails(field);
+                    optionElement.css({
+                        width: fieldDetails.width,
+                        height: fieldDetails.height,
+                        left: fieldDetails.x + 1,
+                        top: fieldDetails.y + 3,
+                    });
                 }
             });
-        })
+        });
     });
 </script>
