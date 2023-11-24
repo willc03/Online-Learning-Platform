0cf362b1d20ff852bda065c00794b5f0dc679c76
Will <corkillw@gmail.com>
2023-11-24 23:12:02 +0000

    Add styling and scripting for ordering questions

--- resources/views/components/questions/order.blade.php
+++ resources/views/components/questions/order.blade.php
@@ -3,11 +3,47 @@
     <legend>Order these items correctly:</legend>
     @shuffle($choices)
     <ul id="question-list">
+        {{-- Add styling if it's horizontal to size the buttons --}}
+        @if($direction == "horizontal")
+            <style>
+                #question-list {
+                    display: flex;
+                    justify-content: space-around;
+                }
+                #question-list button {
+                    width: calc({{ 100/count($choices) }}% - 2%)!important;
+                }
+            </style>
+        @endif
         @for($i = 0; $i < count($choices); $i++)
-            <x-components.3d_button type="button" fg_color="#81d4fa" bg_color="#5a94af">{{ $choices[$i] }}</x-components.3d_button>
+            <x-components.3d_button class="draggable-choice" type="button" fg_color="#81d4fa" bg_color="#5a94af">{{ $choices[$i] }}</x-components.3d_button>
         @endfor
     </ul>
 </fieldset>
 
 <x-components.3d_button id="submit-question" type="submit" bg_color="#88A236" fg_color="#B1CA65">Submit</x-components.3d_button>
 
+<script> {{-- Write a script to manage the clicking of correct answers --}}
+    const buttons = $(".option-button");
+
+    function calculateAnswer() {
+        let answer = [];
+        $("#question-list button").each(function(index, element) {
+            answer.push(element.innerText);
+        })
+        $("#answer").prop("value", JSON.stringify(answer));
+    }
+
+    $(function() {
+        $("#question-list").sortable({
+            revert: true,
+            cancel: false,
+            placeholder: "three-d draggable-choice",
+            scrollSensitivity: 100,
+            stop: calculateAnswer
+        });
+        $("ul, li").disableSelection();
+
+        calculateAnswer(); // Calculate the initial answer
+    });
+</script>
