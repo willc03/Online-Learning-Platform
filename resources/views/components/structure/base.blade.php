1e3762a95989b164b95ff88bbb87d8ca9142949d
Will <corkillw@gmail.com>
2023-11-27 10:57:04 +0000

    Add the CSRF token to meta tags

--- resources/views/components/structure/base.blade.php
+++ resources/views/components/structure/base.blade.php
@@ -5,6 +5,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> {{-- This viewport tag allows for mobile optimisation. --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}"> {{-- From the Laravel website for use in AJAX requests --}}
         <title>{{ $title }} - Online Learning Platform</title> {{-- A title can be provided when the component is called --}}
         <link rel="stylesheet" type="text/css" href="{{ asset('style.css') }}"> {{-- Use a global stylesheet --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
