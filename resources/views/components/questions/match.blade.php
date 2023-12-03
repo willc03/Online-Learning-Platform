14746fdb5573f5cd1c822041224013c537f7423d
Will <corkillw@gmail.com>
2023-12-03 13:31:18 +0000

    Convert match questions to external script

--- resources/views/components/questions/match.blade.php
+++ resources/views/components/questions/match.blade.php
@@ -48,57 +48,6 @@
 </fieldset>
 
 <script>
-    const leftBoxButtons = $(".left-box .three-d");
-    const rightBoxButtons = $(".right-box .three-d");
-    {{-- Set all the boxes to be the same size --}}
-    function resizeButtons() {
-        let maxHeight = 0;
-        $(".three-d").css("height", "").each(function() {
-            maxHeight = $(this).height() > maxHeight ? $(this).height() : maxHeight;
-        }).height(maxHeight + 10).data("extended", true);
-    }
-    resizeButtons();
-
-    $(window).on("resize", resizeButtons);
-    {{-- Manage the matching of items --}}
-    function onButtonClick(buttonGroup, currentButton) {
-        let selectedButtons = $(buttonGroup).filter(".selected").removeClass("selected");
-        if (selectedButtons[0] !== currentButton) {
-            $(currentButton).addClass("selected");
-        }
-
-        if ($(".left-box .three-d.selected").length && $(".right-box .three-d.selected").length) {
-            let leftElement = $(".left-box .three-d.selected").first();
-            let rightElement = $(".right-box .three-d.selected").first();
-            setTimeout(() => $(leftBoxButtons).add(rightBoxButtons).removeClass("selected"), 175);
-
-            $.ajax({
-                headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-                method: "POST",
-                url: "{{ route('question.partial') }}",
-                data: {
-                    'question_id': $("#question_id").val(),
-                    'answer': [$(leftElement).text(), $(rightElement).text()]
-                }
-            }).done(function(data) {
-                var elements = $(leftElement).add(rightElement);
-
-                if (data === "true") {
-                    elements.addClass("correct").prop('disabled', true);
-                } else {
-                    elements.addClass("incorrect");
-                    setTimeout(function() {
-                        elements.removeClass("incorrect");
-                    }, 175);
-                }
-            });
-        }
-    }
-
-    $(leftBoxButtons).on("click", function() {
-        onButtonClick(leftBoxButtons, this);
-    });
-    $(rightBoxButtons).on("click", function() {
-        onButtonClick(rightBoxButtons, this);
-    });
+    ajaxRoute = "{{ route("question.partial") }}";
 </script>
+<script src="{{ asset("assets/scripts/question_scripts/match.js") }}"></script>
