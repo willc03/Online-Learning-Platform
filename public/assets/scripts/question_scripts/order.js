ac297c9f8fd13b45c8e0d50517ee92f73a35807a
Will <corkillw@gmail.com>
2023-12-03 13:38:25 +0000

    Convert order question to external script

---
+++ public/assets/scripts/question_scripts/order.js
@@ -0,0 +1,22 @@
+const buttons = $(".option-button");
+
+function calculateAnswer() {
+    let answer = [];
+    $("#question-list button").each(function(index, element) {
+        answer.push(element.innerText);
+    })
+    $("#answer").prop("value", JSON.stringify(answer));
+}
+
+$(function() {
+    $("#question-list").sortable({
+        revert: true,
+        cancel: false,
+        placeholder: "three-d draggable-choice",
+        scrollSensitivity: 100,
+        stop: calculateAnswer
+    });
+    $("ul, li").disableSelection();
+
+    calculateAnswer(); // Calculate the initial answer
+});
--- resources/views/components/questions/order.blade.php
+++ resources/views/components/questions/order.blade.php
@@ -23,27 +23,4 @@
 
 <x-components.3d_button id="submit-question" type="submit" bg_color="#88A236" fg_color="#B1CA65">Submit</x-components.3d_button>
 
-<script> {{-- Write a script to manage the clicking of correct answers --}}
-    const buttons = $(".option-button");
-
-    function calculateAnswer() {
-        let answer = [];
-        $("#question-list button").each(function(index, element) {
-            answer.push(element.innerText);
-        })
-        $("#answer").prop("value", JSON.stringify(answer));
-    }
-
-    $(function() {
-        $("#question-list").sortable({
-            revert: true,
-            cancel: false,
-            placeholder: "three-d draggable-choice",
-            scrollSensitivity: 100,
-            stop: calculateAnswer
-        });
-        $("ul, li").disableSelection();
-
-        calculateAnswer(); // Calculate the initial answer
-    });
-</script>
+<script src="{{ asset("assets/scripts/question_scripts/order.js") }}"></script>
