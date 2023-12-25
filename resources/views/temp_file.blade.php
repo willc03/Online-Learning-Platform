c081b88ffa057290e9c866dc4bd39689f3b65b65
Will <corkillw@gmail.com>
2023-12-25 23:56:42 +0000

    Create the general form and script for uploading files

--- resources/views/temp_file.blade.php
+++ resources/views/temp_file.blade.php
@@ -1,25 +1,59 @@
-@php
-    session()->flash('course', '9ac2e12e-2bda-4314-b639-e249841f4f7c');
-@endphp
+<x-structure.wrapper title="test">
+    <form method="post" enctype="multipart/form-data" id="basic-file-upload">
+        {{-- A basic file upload form will be used to allow for unique styling on different pages --}}
+        @csrf
+        @method('POST')
 
-@if(session()->has('success'))
-    <h4>FILE UPLOAD SUCCESSFUL</h4>
-@endif
+        <input type="hidden" name="id" id="course-id" value="{{ explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2] }}">
 
+        <fieldset>
+            <legend>File upload:</legend>
 
-<form method="post" action="{{ route('temp.upload') }}" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
+            <label for="name">File name:</label>
+            <input type="text" name="name" id="file-upload-name" required>
 
-    <label for="name">File name:</label>
-    <input type="text" name="name" required>
+            <label for="file">Upload a file:</label>
+            <input type="file" name="file" id="file-upload-slot" required>
 
-    <br>
+            <input type="submit">
+        </fieldset>
 
-    <label for="file">Upload a file:</label>
-    <input type="file" name="file" required>
+        <script>
+            $(function() {
+                {{-- Constants and variables --}}
+                const form = document.getElementById("basic-file-upload");
+                const formFile = document.getElementById("file-upload-slot");
+                const fileName = document.getElementById("file-upload-name");
+                const courseId = document.getElementById("course-id");
+                {{-- Functions --}}
+                function uploadFile() {
+                    let fileContainer = new FormData();
+                    fileContainer.append('id', $(courseId).val());
+                    fileContainer.append('name', $(fileName).val())
+                    fileContainer.append('file', formFile.files[0]);
 
-    <br>
+                    $.ajax({
+                        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                        method: "post",
+                        url: "{{ url('course/'.explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2]) }}/upload", {{-- https://speedysense.com/php-remove-http-https-www-and-slashes-from-url/ --}}
+                        data: fileContainer,
+                        dataType: 'json',
+                        processData: false,
+                        contentType: false,
+                        success: function(data) {
+                            console.log(data);
+                        }
+                    });
+                }
+                {{-- General scripting --}}
+                $(form).on("submit", function( event ) {
+                    event.preventDefault();
 
-    <input type="submit">
-</form>
+                    if ($(form).valid()) { {{-- jQuery validation plugin used for form validation check --}}
+                        uploadFile();
+                    }
+                })
+            });
+        </script>
+    </form>
+</x-structure.wrapper>
