8601a34274338007c49c9a69144f1a5a9ca95db5
Will <corkillw@gmail.com>
2023-12-26 16:20:10 +0000

    Finalise general-purpose file upload form

---
+++ public/assets/scripts/forms/file_upload.js
@@ -0,0 +1,40 @@
+$(function() {
+    // Constants and variables
+    const form = document.getElementById("basic-file-upload");
+    const formFile = document.getElementById("file-upload-slot");
+    const fileName = document.getElementById("file-upload-name");
+    const courseId = document.getElementById("course-id");
+    const messageBox = document.getElementById("file-upload-message-box");
+    const messageMsg = document.getElementById("file-upload-message")
+    // Functions
+    function uploadFile() {
+        let fileContainer = new FormData();
+        fileContainer.append('id', $(courseId).val());
+        fileContainer.append('name', $(fileName).val())
+        fileContainer.append('file', formFile.files[0]);
+
+        $(messageBox).css("display", "none");
+        $.ajax({
+            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            method: "post",
+            url: upload_url,
+            data: fileContainer,
+            dataType: 'json',
+            processData: false,
+            contentType: false,
+            success: function(data) {
+                $(messageMsg).text("File upload " + (data[0] !== true ? "un" : "") + "successful");
+                $(messageBox).css("display", "block");
+                $(messageBox).attr("message-type", (data[0] === true ? "success" : "error"));
+            }
+        });
+    }
+    // General scripting
+    $(form).on("submit", function( event ) {
+        event.preventDefault();
+
+        if ($(form).valid()) { // jQuery validation plugin used for form validation check
+            uploadFile();
+        }
+    })
+});
--- resources/views/temp_file.blade.php
+++ resources/views/temp_file.blade.php
@@ -7,6 +7,9 @@
         <input type="hidden" name="id" id="course-id" value="{{ explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2] }}">
 
         <fieldset>
+            <div class="message" id="file-upload-message-box" style="display: none">
+                <p id="file-upload-message"></p>
+            </div>
             <legend>File upload:</legend>
 
             <label for="name">File name:</label>
@@ -19,41 +22,8 @@
         </fieldset>
 
         <script>
-            $(function() {
-                {{-- Constants and variables --}}
-                const form = document.getElementById("basic-file-upload");
-                const formFile = document.getElementById("file-upload-slot");
-                const fileName = document.getElementById("file-upload-name");
-                const courseId = document.getElementById("course-id");
-                {{-- Functions --}}
-                function uploadFile() {
-                    let fileContainer = new FormData();
-                    fileContainer.append('id', $(courseId).val());
-                    fileContainer.append('name', $(fileName).val())
-                    fileContainer.append('file', formFile.files[0]);
-
-                    $.ajax({
-                        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-                        method: "post",
-                        url: "{{ url('course/'.explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2]) }}/upload", {{-- https://speedysense.com/php-remove-http-https-www-and-slashes-from-url/ --}}
-                        data: fileContainer,
-                        dataType: 'json',
-                        processData: false,
-                        contentType: false,
-                        success: function(data) {
-                            console.log(data);
-                        }
-                    });
-                }
-                {{-- General scripting --}}
-                $(form).on("submit", function( event ) {
-                    event.preventDefault();
-
-                    if ($(form).valid()) { {{-- jQuery validation plugin used for form validation check --}}
-                        uploadFile();
-                    }
-                })
-            });
+            upload_url = "{{ url('course/'.explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2]) }}/upload"; {{-- https://speedysense.com/php-remove-http-https-www-and-slashes-from-url/ --}}
         </script>
+        <script src="{{ asset("assets/scripts/forms/file_upload.js") }}"></script>
     </form>
 </x-structure.wrapper>
