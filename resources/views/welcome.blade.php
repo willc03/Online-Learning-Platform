50e1c6c76c88522fabbe487104468dde5219e009
Will <corkillw@gmail.com>
2023-12-31 14:01:30 +0000

    Add error message display to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
 <x-structure.wrapper title="welcome">
-
+    @if($errors->any())
+        <x-messages.error title="Unexpected Error" description="" :passed_errors="$errors->all()" />
+    @endif
 </x-structure.wrapper>
