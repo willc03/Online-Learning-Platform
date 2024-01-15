2bfe23a554adf2c0dad9ed3db9f93597afacd89f
Will <corkillw@gmail.com>
2024-01-15 16:55:31 +0000

    Structure front-end page

--- resources/views/courses/invite.blade.php
+++ resources/views/courses/invite.blade.php
@@ -1,16 +1,26 @@
 <x-structure.base title="Invite">
     @if(!$success)
-        <div id="invalid-invite-box">
-            <h1>404</h1>
-            <h2>Invite Unavailable</h2>
+        <div id="invite-box">
+            <h2>404</h2>
+            <h3>Invite Unavailable</h3>
             <p>{{ $errorMessage }}</p>
             <x-components.3d_button id="return-home" fg_color="#B1CA65" bg_color="#88A236" onclick="location.href = '{{ url(route('home')) }}'">Return to home</x-components.3d_button>
         </div>
     @else
-
+        <div id="invite-box">
+            <h2>Course Invitation</h2>
+            <h3>You have received an invite to <strong>{{ $content->title }}</strong></h3>
+            <p>Choose one of the options below to accept or decline the invitation.</p>
+            <div class="button-box">
+                <x-components.3d_button id="return-home" fg_color="#CA6565" bg_color="#A23636" onclick="location.href = '{{ url(route('home')) }}'">Return to home</x-components.3d_button>
+                <form method="post" action="{{ url(route('join.accept')) }}">
+                    @csrf
+                    <input type="hidden" name="id" value="{{ $_GET['id'] }}">
+                    <x-components.3d_button id="accept" fg_color="#B1CA65" bg_color="#88A236" onclick="location.href = '{{ url(route('home')) }}'">Accept</x-components.3d_button>
+                </form>
+            </div>
+        </div>
     @endif
 
-    <script>
-        $(document.body).css('overflow', 'hidden');
-    </script>
+    <script src="{{ asset("assets/scripts/disable-elastic-scroll.js") }}"></script>
 </x-structure.base>
