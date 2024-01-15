3bac3c6b6b4a500b7ed0ee0d0faad44f6b1af9de
Will <corkillw@gmail.com>
2024-01-15 20:12:05 +0000

    Remove course checks, replace with middleware

--- app/Http/Controllers/Course/Course.php
+++ app/Http/Controllers/Course/Course.php
@@ -17,14 +17,7 @@ class Course extends Controller
     {
         // Check for a valid course
         $url_course_id = explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2];
-        if (!$url_course_id || !$course = CourseModel::find($url_course_id)) {
-            return redirect()->to(route('home'))->withErrors(['INVALID_COURSE' => 'Course Error - The requested course could not be found.']);
-        }
-        // Check the user is a valid member of the course
-        $user_id = $request->user()->id;
-        if (!UserCourse::where(['course_id' => $url_course_id, 'user_id' => $user_id])->exists() && $user_id !== $course->owner) {
-            return redirect()->to(route('home'))->withErrors(['NOT_COURSE_MEMBER' => 'Course Error - You aren\'t a member of this course!']);
-        }
+        $course = CourseModel::find($url_course_id);
         // Present the course home page to the user
         return view('courses.home', [
             'course_name' => $course->title,
