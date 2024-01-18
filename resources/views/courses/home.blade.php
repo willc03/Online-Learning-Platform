055bda059bf22cb106298c9f0469e2f68c51b23d
Will <corkillw@gmail.com>
2024-01-18 23:53:37 +0000

    Add the start of collapsable course sections

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -13,10 +13,36 @@
     @endif
     {{-- Display all the course content in a downwards fashion --}}
     <h2>Course content</h2>
-    @foreach($course_sections as $course_section)
-        <div class="section" id="{{ $course_section->id }}">
-            <h3>{{ $course_section->title }}</h3>
-        </div>
-    @endforeach
-    {{ $course_sections }}
+    <div id="course-sections">
+        @foreach($course->sections as $course_section)
+            <div class="section" id="{{ $course_section->id }}">
+                <button class="collapse-button" id="{{ $course_section->id }}">{{ $course_section->title }}</button>
+                <div class="collapse-section" id="{{ $course_section->id }}">
+                    <h3>{{ $course_section->title }}</h3>
+                    @foreach($course_section->items as $section_item)
+                        @switch($section_item->item_type)
+                            @case("LESSON")
+                                <h4>LESSON</h4>
+                                <p>{{ $section_item }}</p>
+                                @break
+                        @endswitch
+                    @endforeach
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <script>
+        $(".section .collapse-button").on("click", function() {
+            var contentElement = $(this).next();
+            var isCollapsed = contentElement.hasClass("collapsed");
+
+            $(this).toggleClass("collapsed");
+            contentElement.toggleClass("collapsed").animate({
+                height: isCollapsed ? contentElement.prop('scrollHeight') + "px" : "0px",
+                paddingTop: isCollapsed ? "10px" : "0",
+                paddingBottom: isCollapsed ? "10px" : "0"
+            }, 1000);
+        });
+    </script>
 </x-structure.wrapper>
