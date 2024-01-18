4454019d0bf2466b54eadeb79715f2d4073df696
Will <corkillw@gmail.com>
2024-01-18 21:30:57 +0000

    Add course owners to access middleware

--- app/Http/Middleware/Course.php
+++ app/Http/Middleware/Course.php
@@ -19,11 +19,16 @@ class Course
     public function handle(Request $request, Closure $next): Response
     {
         $urlCourseId = explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2];
-        if (!CourseModel::where('id', $urlCourseId)->exists()) {
+
+        $courseQuery = CourseModel::where('id', $urlCourseId);
+
+        if (!$courseQuery->exists()) {
             return redirect()->to(route('home'))->withErrors(['INVALID_COURSE' => 'The specified course does not exist. Please try again.']);
         }
-        $userTakesCourse = UserCourse::where(['course_id' => $urlCourseId, 'user_id' => $request->user()->id])->exists();
 
-        return $userTakesCourse ? $next($request) : redirect()->to(route('home'))->withErrors(['NOT_COURSE_MEMBER' => 'Please join this course to access its pages.']);
+        $userTakesCourse = UserCourse::where(['course_id' => $urlCourseId, 'user_id' => $request->user()->id])->exists();
+        $userOwnsCourse = $courseQuery->firstOrFail()->owner === $request->user()->id;
+
+        return ($userTakesCourse || $userOwnsCourse) ? $next($request) : redirect()->to(route('home'))->withErrors(['NOT_COURSE_MEMBER' => 'Please join this course to access its pages.']);
     }
 }
