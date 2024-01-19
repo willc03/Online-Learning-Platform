7602667e131c2139f792663a9994749e2d93fbab
Will <corkillw@gmail.com>
2024-01-19 18:44:15 +0000

    Add debounce for the collapsing

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -37,12 +37,19 @@
             var contentElement = $(this).next();
             var isCollapsed = contentElement.hasClass("collapsed");
 
+            if ($(contentElement).hasClass("collapsing")) {
+                return;
+            }
+            $(contentElement).addClass("collapsing");
+
             $(this).toggleClass("collapsed");
             contentElement.toggleClass("collapsed").animate({
                 height: isCollapsed ? contentElement.prop('scrollHeight') + "px" : "0px",
                 paddingTop: isCollapsed ? "10px" : "0",
                 paddingBottom: isCollapsed ? "10px" : "0"
-            }, 1000);
+            }, 1000, function() {
+                $(contentElement).removeClass("collapsing");
+            });
         });
     </script>
 </x-structure.wrapper>
