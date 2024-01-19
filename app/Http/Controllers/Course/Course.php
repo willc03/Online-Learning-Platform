ae865c05ddba390ca1e32e1d69c573ee3b4f1da2
Will <corkillw@gmail.com>
2024-01-19 23:05:49 +0000

    Add server-side processing for reordering

--- app/Http/Controllers/Course/Course.php
+++ app/Http/Controllers/Course/Course.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Course as CourseModel;
 use App\Models\User;
 use App\Models\UserCourse;
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 // 'as' used due to duplicate definition of key word 'Course'
 
@@ -24,5 +26,38 @@ class Course extends Controller
             'owner' => User::find($course->owner),
             'user_is_owner' => ($request->user()->id === $course->owner)
         ]);
+    }
+
+    // Add a function to allow AJAX requests to edit
+    public function contentEdit(Request $request) {
+        // Run validation
+        $validated_data = $request->validate([
+            'course_id' => ['string', 'required', 'exists:courses,id'],
+            'edit_type' => ['string', 'required'],
+            'data' => ['required', 'json']
+        ]);
+        $validated_data['data'] = Json::decode($validated_data['data']);
+        // Get course
+        $course = CourseModel::find($validated_data['course_id']);
+        // Ensure gate permissions met
+        if (!Gate::allows('course-edit', $course)) {
+            return [false, '403'];
+        }
+        // Make edits based on type
+        switch ( $validated_data['edit_type'] ) {
+            case "section_order":
+                $course_sections = $course->sections;
+                foreach ($course_sections as $section) {
+                    foreach ($validated_data['data'] as $data) {
+                        if ($data[1] == $section->id) {
+                            $section->position = $data[0];
+                            $section->save();
+                        }
+                    }
+                }
+                break;
+        }
+        // Returns
+        return 200;
     }
 }
