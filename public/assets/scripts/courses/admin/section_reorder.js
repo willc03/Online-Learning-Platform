5b0b990965f9c117a7456bc609046976b7cc6f2c
Will <corkillw@gmail.com>
2024-01-22 18:11:44 +0000

    Make re-ordering an external script

---
+++ public/assets/scripts/courses/admin/section_reorder.js
@@ -0,0 +1,65 @@
+// Define constants to be used throughout the script
+const collapseButtons = $(".section .collapse-button");
+const collapseSections = $(collapseButtons).next();
+// Create the sorter
+const sectionSorter = $("#course-sections").sortable({
+    revert: true,
+    placeholder: "course_section_placeholder",
+    opacity: 0.5,
+    cancel: false,
+});
+// Disable the sorter by default
+$(sectionSorter).sortable("disable");
+// Handle button clicks
+$("#reorder-sections-button").on("click", function () {
+    if ($(this).attr("data-active") === "false") {
+        $("#reorder-sections-button span").text('Save new order').animate({backgroundColor: '#B1CA65'}, 500);
+        $("#reorder-sections-button").animate({backgroundColor: '#88A236'}, 500);
+        // Collapse all the sections
+        $(collapseSections).animate({
+            height: "0px",
+            paddingTop: "0px",
+            paddingBottom: "0px"
+        }, 1000, function () {
+            $(collapseSections).addClass("collapsing");
+            $(collapseButtons).addClass("collapsed").animate({
+                borderRadius: "8px"
+            }, 1000);
+            // Enable the sorter
+            $(sectionSorter).sortable("enable");
+            $("#reorder-sections-button").attr("data-active", "true")
+        });
+    } else {
+        // Disable the sorter
+        $("#reorder-sections-button span").text('Re-order sections').animate({backgroundColor: '#a491d3'}, 500);
+        $("#reorder-sections-button").animate({backgroundColor: '#7a5fbf'}, 500);
+        $(sectionSorter).sortable("disable");
+        // Un-collapse the sections
+        $(collapseSections).each(function () {
+            $(this).animate({
+                height: $(this).prop("scrollHeight") + "px",
+                paddingTop: "10px",
+                paddingBottom: "10px"
+            }, 1000);
+        }).removeClass("collapsing");
+        $(collapseButtons).removeClass("collapsed").css("borderRadius", "");
+        $("#reorder-sections-button").attr("data-active", "false");
+        // AJAX request for setting the new order
+        let order = [];
+        $("div .section.draggable-choice").each(function (index) {
+            order.push([index + 1, $(this).attr("id")])
+        });
+        let orderJson = JSON.stringify(order);
+
+        $.ajax({
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            method: "POST",
+            url: ajaxRoute,
+            data: {
+                'course_id': courseId,
+                'edit_type': 'section_order',
+                'data': orderJson
+            }
+        });
+    }
+})
