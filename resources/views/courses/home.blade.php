e9fcc938d99b75c79bc2b83e5a38d6ef592d63c5
Will <corkillw@gmail.com>
2024-01-22 22:58:45 +0000

    Add client-side AJAX script for new sections

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -56,6 +56,32 @@
                     </div>
                 </fieldset>
             </form>
+            <script>
+                const addSectionForm = $('#course-section-add-form');
+                $(addSectionForm)
+                    .data('size', $(addSectionForm).height())
+                    .css('height', '0')
+                    .submit(function( event ) {
+                        event.preventDefault();
+                        $.ajax({
+                            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                            method: "POST",
+                            url: ajaxRoute,
+                            data: {
+                                'course_id': courseId,
+                                'edit_type': 'new_section',
+                                'data': JSON.stringify($(this).serializeArray())
+                            },
+                            success: function(data) {
+                                if (data === 'SUCCESS') {
+                                    location.href = location.href;
+                                }
+                            }
+                        });
+                    });
+            </script>
+        @endif
+    </div>
 
     <script src="{{ asset("assets/scripts/courses/collapse_sections.js") }}"></script>
 
