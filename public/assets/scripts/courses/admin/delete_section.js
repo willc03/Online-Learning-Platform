da3b763d657f4bd22ddb954f63b3ef9ef9c8d775
Will <corkillw@gmail.com>
2024-01-23 13:47:48 +0000

    Separate files for deletion

---
+++ public/assets/scripts/courses/admin/delete_section.js
@@ -0,0 +1,43 @@
+$(".three-d.course-button-mini#delete-button").on("click", function() {
+    const button = $(this);
+    const foreground = $(button).children('span')[0];
+
+    if ($(button).data("shouldDelete") === true) {
+        $.ajax({
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            method: "POST",
+            url: ajaxRoute,
+            data: {
+                'course_id': courseId,
+                'edit_type': 'delete_section',
+                'data': JSON.stringify({'section_id': $(button).parent().attr('id')})
+            },
+            success: function(data) {
+                if (data === 'SUCCESS') {
+                    let section = $(button).parent();
+                    let section_button = $(section).prev();
+                    let section_container = $(section).parent()
+
+                    $(section_container).css('background', 'none');
+                    $(section).animate({height: 0, padding: 0}, 500, function() {
+                        $(section_button).css('overflow', 'hidden').animate({height: 0, padding: 0}, 500, function() {
+                            $(section_container).animate({padding: 0, margin: 0}, 500, function() {
+                                $(section_container).remove();
+                            });
+                        })
+                    })
+                }
+            }
+        });
+    } else {
+        $(button).animate({backgroundColor: "#88A236"}, 500);
+        $(foreground).animate({backgroundColor: "#B1CA65"}, 500).text("Confirm");
+        $(button).data("shouldDelete", true);
+
+        setTimeout(function() {
+            $(button).data("shouldDelete", false);
+            $(button).animate({backgroundColor: $(button).attr("bg-color") || $(button).attr("bg_color") || "#ffffff"}, 500);
+            $(foreground).animate({backgroundColor: $(button).attr("fg-color") || $(button).attr("fg_color") || "#ffffff"}, 500).text("Delete section");
+        }, 7500);
+    }
+});
--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -50,40 +50,7 @@
         <x-courses.add_section />
         <script src="{{ asset('assets/scripts/courses/admin/add_section.js') }}"></script>
 
-        <script>
-            $(".three-d.course-button-mini#delete-button").on("click", function() {
-                const button = $(this);
-                const foreground = $(button).children('span')[0];
-
-                if ($(button).data("shouldDelete") === true) {
-                    $.ajax({
-                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                        method: "POST",
-                        url: ajaxRoute,
-                        data: {
-                            'course_id': courseId,
-                            'edit_type': 'delete_section',
-                            'data': JSON.stringify({'section_id': $(button).parent().attr('id')})
-                        },
-                        success: function(data) {
-                            if (data === 'SUCCESS') {
-
-                            }
-                        }
-                    });
-                } else {
-                    $(button).animate({backgroundColor: "#88A236"}, 500);
-                    $(foreground).animate({backgroundColor: "#B1CA65"}, 500).text("Confirm");
-                    $(button).data("shouldDelete", true);
-
-                    setTimeout(function() {
-                        $(button).data("shouldDelete", false);
-                        $(button).animate({backgroundColor: $(button).attr("bg-color") || $(button).attr("bg_color") || "#ffffff"}, 500);
-                        $(foreground).animate({backgroundColor: $(button).attr("fg-color") || $(button).attr("fg_color") || "#ffffff"}, 500).text("Delete section");
-                    }, 7500);
-                }
-            })
-        </script>
+        <script src="{{ asset('assets/scripts/courses/admin/delete_section.js') }}"></script>
     @endif
 
     <script src="{{ asset("assets/scripts/courses/collapse_sections.js") }}"></script>
