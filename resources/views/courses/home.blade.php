134d4d19f212dfcdc3ab74f11c33f394ed8a1043
Will <corkillw@gmail.com>
2024-01-25 13:07:56 +0000

    Style lesson component of section

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -24,6 +24,7 @@
                 <div class="collapse-section" id="{{ $course_section->id }}">
                     {{-- Add deletion buttons if the user is a course admin and is editing --}}
                     @if ($user_is_owner && $is_editing)
+                        <h4>Admin Controls</h4>
                         <x-components.3d_button class="course-button-mini" id="delete-button" fg_color="#CA6565" bg_color="#A23636">Delete section</x-components.3d_button>
                     @endif
                     {{-- Add description --}}
@@ -35,8 +36,13 @@
                         {{-- Add the item accordingly with a switch-case --}}
                         @switch($section_item->item_type)
                             @case("LESSON")
-                                <h4>LESSON</h4>
-                                <p>{{ $section_item }}</p>
+                                <div class="lesson">
+                                    <h5>{{ $section_item->title }}</h5>
+                                    @if($section_item->description != null)
+                                        <p>{{ $section_item->description }}</p>
+                                    @endif
+                                    <x-components.3d_button fg-color="#9EC5AB" bg-color="#5e9c73" onclick="location.href = '{{ url()->current() }}'">BEGIN LESSON</x-components.3d_button>
+                                </div>
                                 @break
                         @endswitch
                     @endforeach
@@ -48,18 +54,17 @@
     {{-- If the user is an admin, allow them to add additional sections --}}
     @if($user_is_owner && $is_editing)
         <x-courses.add_section />
-        <script src="{{ asset('assets/scripts/courses/admin/add_section.js') }}"></script>
-
-        <script src="{{ asset('assets/scripts/courses/admin/delete_section.js') }}"></script>
     @endif
 
     <script src="{{ asset("assets/scripts/courses/collapse_sections.js") }}"></script>
 
-    @if ($is_editing)
+    @if ($is_editing) {{-- Add the scripts --}}
         <script>
             ajaxRoute = '{{ url(route('course.edit', ['id' => $course->id])) }}';
             courseId = '{{ $course->id }}';
         </script>
         <script src="{{ asset("assets/scripts/courses/admin/section_reorder.js") }}"></script>
+        <script src="{{ asset('assets/scripts/courses/admin/add_section.js') }}"></script>
+        <script src="{{ asset('assets/scripts/courses/admin/delete_section.js') }}"></script>
     @endif
 </x-structure.wrapper>
