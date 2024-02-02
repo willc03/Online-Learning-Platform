106b04378f362946827c15c9420240a3963275d4
Will <corkillw@gmail.com>
2024-02-02 19:03:50 +0000

    Add a button and div for adding components

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -32,9 +32,14 @@
                         <div class="section-admin-panel">
                             <h4>Admin Controls</h4>
                             <x-components.3d_button class="course-button-mini" id="delete-button" fg_color="#CA6565" bg_color="#A23636">Delete section</x-components.3d_button>
+                            <x-components.3d_button class="max-content course-button-mini" fg-color="#9EC5AB" bg-color="#5e9c73">Add new component</x-components.3d_button>
                             @if ($course_section->items->count() > 1)
                                 <x-components.3d_button class="course-button-mini" id="reorder-section-button" fg-color="#9EC5AB" bg-color="#5e9c73" data-active="false">Re-order components</x-components.3d_button>
                             @endif
+                        </div>
+                        {{-- Display the form to add new sections --}}
+                        <div id="section-add-component">
+                            <x-courses.add_component course_id="{{ $course->id }}" section_id="{{ $course_section->id }}" />
                         </div>
                     @endif
                     {{-- Add description --}}
