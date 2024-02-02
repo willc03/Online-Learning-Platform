40cc1852ca7e2ee5a096688949caee34ebf42d06
Will <corkillw@gmail.com>
2024-02-02 23:54:20 +0000

    Add a script to manage form visuals for client

--- resources/views/courses/home.blade.php
+++ resources/views/courses/home.blade.php
@@ -78,8 +78,97 @@
         <x-courses.add_section />
     @endif
 
-    <script src="{{ asset("assets/scripts/courses/collapse_sections.js") }}"></script>
+    <script>
+        // Form requests
+        $(".section select[name='type']").change(function() {
+            // Get the necessary data for the section
+            let section = $(this).closest('.section');
+            let selectedFormType = $(this).val();
+            let formContainer = $(section).find('#form_container');
+            let submissionContainer = $(section).find('#submission');
+            // Empty the form container
+            $(formContainer).empty();
+            // Make the AJAX request to re-populate the form
+            $.ajax({
+                url: '{{ route("course.getForm", ["id" => $course->id]) }}',
+                data: {
+                    form_type: selectedFormType,
+                    course_id: '{{ $course->id }}',
+                    section_id: $(section).attr('id')
+                },
+                success: function(data) {
+                    $(formContainer).html(data).css('display', '');
+                    $(submissionContainer).css('display', '');
+                }
+            });
+        });
 
+        // Default container setup
+        $('.section .section-add-component').each(function() {
+            // Define necessary components
+            let additionContainer = $(this);
+            // Default behaviour
+            $(additionContainer)
+                .data('size', $(additionContainer).height())
+                .data('is_open', false)
+                .css({
+                    height: 0,
+                    borderWidth: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0
+                });
+        });
+
+        // Addition button click
+        $('.section .section-admin-panel #add-component-button').each(function() {
+            // Define necessary components
+            let button = $(this);
+            let section = $(this).closest('.section');
+            let additionContainer = $(section).find('.section-add-component');
+            // Button click behaviour
+            $(button).on('click', function() {
+                if ($(additionContainer).data('is_open') === true) { return; }
+                $(additionContainer)
+                    .data('is_open', true)
+                    .animate({
+                    height: $(additionContainer).data('size') + 'px',
+                    borderWidth: '1.5px',
+                    paddingTop: '10px',
+                    paddingBottom: '10px'
+                }, 500, function() {
+                    $(additionContainer).css('height', '');
+                });
+            });
+        });
+
+        // Addition cancellation
+        $('.section #submission #cancel').each(function() {
+            // Define necessary components
+            let button = $(this);
+            let section = $(this).closest('.section');
+            let additionContainer = $(section).find('.section-add-component');
+            let formContainer = $(section).find('#form_container');
+            let submissionContainer = $(section).find('#submission');
+            // Button click behaviour
+            $(button).on('click', function() {
+                if ($(additionContainer).data('is_open') === false) { return; }
+                $(additionContainer)
+                    .data('is_open', false)
+                    .animate({
+                    height: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0,
+                    borderWidth: 0
+                }, 500, function() {
+                    $(formContainer).empty().css('display', 'none');
+                    $(submissionContainer).css('display', 'none');
+                });
+            });
+        });
+    </script>
+
+
+    <script src="{{ asset("assets/scripts/courses/collapse_sections.js") }}"></script>
     @if ($is_editing) {{-- Add the scripts --}}
         <script>
             ajaxRoute = '{{ url(route('course.edit', ['id' => $course->id])) }}';
