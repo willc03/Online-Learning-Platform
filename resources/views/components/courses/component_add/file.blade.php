f48f6ad3a039ce74d773ecba83bdb786296a3c4c
Will <corkillw@gmail.com>
2024-02-11 22:08:45 +0000

    Edit file component addition form

--- resources/views/components/courses/component_add/file.blade.php
+++ resources/views/components/courses/component_add/file.blade.php
@@ -8,8 +8,9 @@
             Title:
             <input type="text" name="title" required>
         </label>
+        <br>
         <label>
-            Select a file from those in your course's file store.<br>Don't see a file you need? <a href="{{ route('course.settings', ['id' => $courseId]) }}">Upload a new file</a>
+            Select a file from those in your course's file store.<br><span class="small-text italicise">Don't see a file you need?</em> <a href="{{ route('course.settings', ['id' => $courseId]) }}">Upload a new file</a></span>
             <br><br>
             <select name="file" style="width: 75%; margin: 0 12.5%;" required>
                 @foreach($course->files as $entry)
