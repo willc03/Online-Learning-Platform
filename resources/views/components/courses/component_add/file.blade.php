fdaf44d1cbfd25414339ca18a5291711c13ce8e7
Will <corkillw@gmail.com>
2024-02-11 22:04:30 +0000

    Update file upload form for course setting page
    (Course settings page to be added later)

--- resources/views/components/courses/component_add/file.blade.php
+++ resources/views/components/courses/component_add/file.blade.php
@@ -4,10 +4,19 @@
     <input type="hidden" name="course-id" value="{{ $courseId }}">
     <input type="hidden" name="section-id" value="{{ $sectionId }}">
     <fieldset>
-        <p>Don't have a file you need? <a href="{{ route('course.settings', ['id' => $courseId]) }}">Upload a new file</a></p>
-        <label class="flex-col">
-            Choose a file to display:
-            <input name="content" type="file">
+        <label>
+            Title:
+            <input type="text" name="title" required>
         </label>
+        <label>
+            Select a file from those in your course's file store.<br>Don't see a file you need? <a href="{{ route('course.settings', ['id' => $courseId]) }}">Upload a new file</a>
+            <br><br>
+            <select name="file" style="width: 75%; margin: 0 12.5%;" required>
+                @foreach($course->files as $entry)
+                    <option value="{{ $entry->id }}">{{ $entry->name }} ({{ basename($entry->path) }})</option>
+                @endforeach
+            </select>
+        </label>
+        <br>
     </fieldset>
 </form>
