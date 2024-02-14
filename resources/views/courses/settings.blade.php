19e27c4eb4b9be1c0921edc03606dde9c8336050
Will <corkillw@gmail.com>
2024-02-14 20:13:42 +0000

    Add file upload and display to settings page
    The files upload form has been moved such that it is displayed on the course settings page. File uploads work the same as previously implemented, however, the next commit will introduce the ability to delete files from the file store

--- resources/views/courses/settings.blade.php
+++ resources/views/courses/settings.blade.php
@@ -24,6 +24,74 @@
             <x-components.3d_button id="details-submit" class="course-button-mini max-content" fg-color="#9EC5AB" bg-color="#5e9c73" disabled>Set new details</x-components.3d_button>
         </form>
     </div>
+    {{-- The file management section for the course will be displayed here, allowing users to upload or remove files as necessary --}}
+    <h2>COURSE FILES</h2>
+    <div id="course-files" class="flex-col">
+        <h3>Upload new files</h3>
+        <form method="post" enctype="multipart/form-data" id="basic-file-upload">
+            {{-- A basic file upload form will be used to allow for unique styling on different pages --}}
+            @csrf
+            @method('POST')
+
+            <input type="hidden" name="id" id="course-id" value="{{ explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2] }}">
+
+            <fieldset class="flex-col">
+                <div class="message" id="file-upload-message-box" style="display: none">
+                    <p id="file-upload-message"></p>
+                </div>
+
+                <label for="name">File name:</label>
+                <input type="text" name="name" id="file-upload-name" required>
+
+                <label for="file">Upload a file:</label>
+                <input type="file" name="file" id="file-upload-slot" required>
+
+                <input type="submit">
+            </fieldset>
+
+            <script>
+                upload_url = "{{ route('course.file.upload', ['id' => $course->id]) }}";
+            </script>
+            <script src="{{ asset("assets/scripts/forms/file_upload.js") }}"></script>
+        </form>
+        <h3>Manage files</h3>
+        @if ($course->files->count() === 0)
+            <p>This course does not have any files! Files will appear here when available.</p>
+        @else
+            <div class="file-table">
+                <div class="table-row table-header">
+                    <div class="table-col">
+                        <p>Public file name</p>
+                    </div>
+                    <div class="table-col">
+                        <p>File name</p>
+                    </div>
+                    <div class="table-col">
+                        <p>Date added</p>
+                    </div>
+                    <div class="table-col">
+                        <p>Action(s)</p>
+                    </div>
+                </div>
+                @foreach($course->files as $file)
+                    <div class="table-row">
+                        <div class="table-col">
+                            <p>{{ $file->name }}</p>
+                        </div>
+                        <div class="table-col">
+                            <p>{{ basename($file->path) }}</p>
+                        </div>
+                        <div class="table-col">
+                            <p>{{ date('d/m/Y H:i', $file->created_at->getTimestamp()) }}</p>
+                        </div>
+                        <div class="table-col">
+                            <x-components.3d_button class="course-button-mini no-buffer" fg_color="#CA6565" bg_color="#A23636">Delete</x-components.3d_button>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+    </div>
     {{-- Invitations will be managed here, if the course is private --}}
     <h2>COURSE INVITATIONS</h2>
     <div id="course-invitations" class="flex-col">
