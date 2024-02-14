8594108581bf17ad37e885180f6985622fd14162
Will <corkillw@gmail.com>
2024-02-14 20:38:51 +0000

    Move scripts to external paths for readability

--- resources/views/courses/settings.blade.php
+++ resources/views/courses/settings.blade.php
@@ -91,48 +91,6 @@
                 @endforeach
             </div>
         @endif
-        <script>
-            ajaxRoute = "{{ route("course.file.remove", ['id' => $course->id]) }}";
-        </script>
-        <script>
-            $("#file-manager .three-d").on("click", function() {
-                const button = $(this);
-                const foreground = $(button).children('span')[0];
-                let row = $(this).closest('.table-row');
-
-                if ($(button).data("shouldDelete") === true) {
-                    $.ajax({
-                        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                        method: "DELETE",
-                        url: ajaxRoute,
-                        data: {
-                            fileId: $(row).attr('id')
-                        },
-                        success: function() {
-                            // Hide the row
-                            $(row).css({
-                                height: $(row).height(),
-                                overflow: 'hidden'
-                            }).animate({
-                                height: 0
-                            }, 500, function() {
-                                $(row).remove();
-                            });
-                        }
-                    });
-                } else {
-                    $(button).animate({backgroundColor: "#88A236"}, 500);
-                    $(foreground).animate({backgroundColor: "#B1CA65"}, 500).text("Confirm Deletion");
-                    $(button).data("shouldDelete", true);
-
-                    setTimeout(function() {
-                        $(button).data("shouldDelete", false);
-                        $(button).animate({backgroundColor: $(button).attr("bg-color") || $(button).attr("bg_color") || "#ffffff"}, 500);
-                        $(foreground).animate({backgroundColor: $(button).attr("fg-color") || $(button).attr("fg_color") || "#ffffff"}, 500).text("Delete");
-                    }, 7500);
-                }
-            });
-        </script>
     </div>
     {{-- Invitations will be managed here, if the course is private --}}
     <h2>COURSE INVITATIONS</h2>
@@ -157,4 +115,8 @@
 
     {{-- Scripts --}}
     <script src="{{ asset("assets/scripts/courses/admin/core_edit.js") }}"></script>
+    <script>
+        ajaxRoute = "{{ route("course.file.remove", ['id' => $course->id]) }}";
+    </script>
+    <script src="{{ asset("assets/scripts/forms/file_delete.js") }}"></script>
 </x-structure.wrapper>
