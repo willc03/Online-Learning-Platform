3010d58188096bc25051b8c60b06f97f291b1256
Will <corkillw@gmail.com>
2024-02-16 15:16:42 +0000

    Change CSS properties

--- public/assets/scripts/courses/admin/settings/invite_max_uses.js
+++ public/assets/scripts/courses/admin/settings/invite_max_uses.js
@@ -13,7 +13,7 @@ $(maxUseToggleButtons).on('click', function() {
     let form = $(box).find("#max-use-form");
     //
     $(button).animate({ height: 0 }, 500, function() { $(this).css('overflow', 'hidden'); });
-    $(form).animate({ height: $(form).data('height') }, 500);
+    $(form).animate({ height: $(form).data('height'), padding: '10px' }, 500);
 });
 // Form editing
 $(toggleSections).each(function() {
@@ -21,7 +21,6 @@ $(toggleSections).each(function() {
     let button = $(this).find("#submit-invite-max-use");
     let row = $(this).closest(".table-row");
     let inviteId = $(row).attr('id');
-    console.log($(input).val())
     $(input).on("input", function() {
         $(button).prop("disabled", $(this).val() === $(this).attr("data-initial"))
     });
