00cfd1602d856e0fa780b9c8c93e875a08bed7b6
Will <corkillw@gmail.com>
2024-03-02 14:36:49 +0000

    Add date picker to invite creation form

--- resources/views/components/courses/add_invite.blade.php
+++ resources/views/components/courses/add_invite.blade.php
@@ -15,7 +15,7 @@
             </label>
             <label>
                 Set maximum number of uses:
-                <input type="number" min="0" name="allowedUses" value="0" style="width: 20%;">
+                <input type="number" min="1" name="allowedUses" value="1" style="width: 20%;">
             </label>
         </fieldset>
 
@@ -27,10 +27,21 @@
             </label>
             <label>
                 Set expiry date:
-                <input type="text" name="expiryDate" value="{{ date("d/m/Y H:i", now()->getTimestamp()) }}">
+                <input id="expiryDate" type="text" name="expiryDate" value="{{ date("d/m/Y H:i", now()->getTimestamp()) }}">
             </label>
         </fieldset>
 
         <x-components.3d_button id="add-invite-btn" class="course-button-mini no-buffer max-content" fg_color="#B1CA65" bg_color="#88A236">Submit</x-components.3d_button>
     </fieldset>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.full.min.js"></script>
+    <script>
+        $("#expiryDate").datetimepicker({
+            minDate: 0,
+            defaultTime: "23:59",
+            formatDate: "d/m/Y",
+            formatTime: "H:i",
+            format: "d/m/Y H:i"
+        });
+    </script>
 </form>
