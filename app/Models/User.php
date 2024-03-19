2b595ee268537e3d9f3bec95c75b927da717638c
Will <corkillw@gmail.com>
2024-03-19 12:45:56 +0000

    Encrypt user's names and emails

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,6 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'name' => 'encrypted',
+        'email' => 'encrypted'
     ];
 
     /*
