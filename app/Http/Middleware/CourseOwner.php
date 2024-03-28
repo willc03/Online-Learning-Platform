6513a5753a158aa671f6458e33b7fc5c6c5cfea8
Will <corkillw@gmail.com>
2024-03-28 17:33:19 +0000

    Tidy up and comment all custom middleware

--- app/Http/Middleware/CourseOwner.php
+++ app/Http/Middleware/CourseOwner.php
@@ -9,21 +9,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CourseOwner
 {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Closure(Request): (Response) $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle ( Request $request, Closure $next ) : Response
     {
-        $urlCourseId = explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2];
-
+        // Get the course id from the URL
+        $urlCourseId = explode('/', preg_replace("#^[^:/.]*[:/]+#i", "", url()->current()))[2];
+        // Get the course from the id
         $courseQuery = Course::where('id', $urlCourseId);
-
-        if (!$courseQuery->exists()) {
-            return redirect()->to(route('home'))->withErrors(['INVALID_COURSE' => 'The specified course does not exist. Please try again.']);
+        // Check the course exists
+        if ( !$courseQuery->exists() ) {
+            return redirect()->to(route('home'))->withErrors([ 'INVALID_COURSE' => 'The specified course does not exist. Please try again.' ]);
         }
-
-        return ($courseQuery->firstOrFail()->owner === $request->user()->id) ? $next($request) : redirect()->to(route('course.home', ['id' => $urlCourseId]))->withErrors(['NOT_COURSE_OWNER' => 'You must be the owner of this course to access this page.']);
+        // Return a redirect or allow the request based on whether the user is part of the course.
+        return ( $courseQuery->firstOrFail()->owner === $request->user()->id ) ? $next($request) : redirect()->to(route('course.home', [ 'id' => $urlCourseId ]))->withErrors([ 'NOT_COURSE_OWNER' => 'You must be the owner of this course to access this page.' ]);
     }
+
 }
