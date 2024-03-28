696256c6067e928ee75a489d119dda87175c1767
Will <corkillw@gmail.com>
2024-03-28 17:33:10 +0000

    Comment and tidy up custom middleware

--- app/Http/Middleware/Course.php
+++ app/Http/Middleware/Course.php
@@ -2,38 +2,40 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\UserCourse;
 use App\Models\Course as CourseModel;
-
+use App\Models\UserCourse;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Course
 {
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Closure(Request): (Response) $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle ( Request $request, Closure $next ) : Response
     {
-        $urlCourseId = explode('/', preg_replace( "#^[^:/.]*[:/]+#i", "", url()->current()) )[2];
-
+        // Get the course id from the url
+        $urlCourseId = explode('/', preg_replace("#^[^:/.]*[:/]+#i", "", url()->current()))[2];
+        // Get the course record by the id
         $courseQuery = CourseModel::where('id', $urlCourseId);
-
-        if (!$courseQuery->exists()) {
-            return redirect()->to(route('home'))->withErrors(['INVALID_COURSE' => 'The specified course does not exist. Please try again.']);
+        // Check the course exists.
+        if ( !$courseQuery->exists() ) {
+            return redirect()->to(route('home'))->withErrors([ 'INVALID_COURSE' => 'The specified course does not exist. Please try again.' ]);
         }
-
-        $userCourseQuery = UserCourse::where(['course_id' => $urlCourseId, 'user_id' => $request->user()->id]);
+        // Get details about the course
+        $userCourseQuery = UserCourse::where([ 'course_id' => $urlCourseId, 'user_id' => $request->user()->id ]);
         $userTakesCourse = $userCourseQuery->exists();
         $userOwnsCourse = $courseQuery->firstOrFail()->owner === $request->user()->id;
-
-        if ($userTakesCourse && $userCourseQuery->firstOrFail()->blocked) {
-            return redirect()->to(route('home'))->withErrors(['BLOCKED' => 'You are blocked from viewing the contents of this course']);
+        // Redirect the user if they're blocked.
+        if ( $userTakesCourse && $userCourseQuery->firstOrFail()->blocked ) {
+            return redirect()->to(route('home'))->withErrors([ 'BLOCKED' => 'You are blocked from viewing the contents of this course' ]);
         }
-
-        return ($userTakesCourse || $userOwnsCourse) ? $next($request) : redirect()->to(route('home'))->withErrors(['NOT_COURSE_MEMBER' => 'Please join this course to access its pages.']);
+        // Redirect the user based on results
+        return ( $userTakesCourse || $userOwnsCourse ) ? $next($request) : redirect()->to(route('home'))->withErrors([ 'NOT_COURSE_MEMBER' => 'Please join this course to access its pages.' ]);
     }
+
 }
