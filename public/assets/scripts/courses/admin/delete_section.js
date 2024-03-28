eafe8f0bd95f7626010d1055c7104ac398ed495c
Will <corkillw@gmail.com>
2024-03-28 22:43:45 +0000

    Update section delete animation

--- public/assets/scripts/courses/admin/delete_section.js
+++ public/assets/scripts/courses/admin/delete_section.js
@@ -7,20 +7,10 @@ $(".three-d.course-button-mini#delete-button").on("click", function () {
             headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }, method: "POST", url: ajaxRoute, data: {
                 'course_id': courseId, 'edit_type': 'delete_section', 'data': JSON.stringify({ 'section_id': $(button).parent().parent().attr('id') })
             }, success: function (data) {
-                if ( data === 'SUCCESS' ) {
-                    let section = $(button).parent().parent();
-                    let section_button = $(section).prev();
-                    let section_container = $(section).parent()
-
-                    $(section_container).css('background', 'none');
-                    $(section).animate({ height: 0, padding: 0 }, 500, function () {
-                        $(section_button).css('overflow', 'hidden').animate({ height: 0, padding: 0 }, 500, function () {
-                            $(section_container).animate({ padding: 0, margin: 0 }, 500, function () {
-                                $(section_container).remove();
-                            });
-                        })
-                    })
-                }
+                let section = $(button).closest("div.section");
+                $(section).css({ overflow: 'hidden', animationFillMode: 'forwards' }).animate({ height: 0, padding: 0, boxShadow: 0 }, 500, function() {
+                    $(section).remove();
+                });
             }
         });
     } else {
