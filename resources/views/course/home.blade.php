3f9e5be184a55dfee2447f3067083ff1a856d2c9
Will <corkillw@gmail.com>
2024-04-04 13:05:46 +0100

    Update variable error

--- resources/views/course/home.blade.php
+++ resources/views/course/home.blade.php
@@ -69,14 +69,14 @@
                                 <legend>Edit section details</legend>
                                 <input type="hidden" name="edit_type" value="section_edit" />
                                 <input type="hidden" name="course_id" value="{{ $course->id }}" />
-                                <input type="hidden" name="section_id" value="{{ $section->id }}" />
+                                <input type="hidden" name="section_id" value="{{ $course_section->id }}" />
                                 <label class="form-flex">
                                     <span>Section Title:</span>
-                                    <input name="title" type="text" value="{{ $section->title }}" required />
+                                    <input name="title" type="text" value="{{ $course_section->title }}" required />
                                 </label>
                                 <label class="form-flex">
                                     <span>Section Description:</span>
-                                    <textarea name="description" style="resize: none">{{ $section->description ?? "" }}</textarea>
+                                    <textarea name="description" style="resize: none">{{ $course_section->description ?? "" }}</textarea>
                                 </label>
                                 <x-ui.interactive-button id="section-details-submit" class="course-button-mini max-content" fg-color="#43AA8B" bg-color="#245B4A" disabled>Set new details</x-ui.interactive-button>
                             </form>
